e1c742fc940f581e38346af14f1efbc8a0d49a40
Kacper Aleksander <122747592+kacperfin@users.noreply.github.com>
2026-03-30 23:36:39 +0200

    Add: string-encode-and-decode - submission-7

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-7.py
@@ -0,0 +1,19 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        len_strs = [str(len(string)) for string in strs]
+        return f"{",".join(len_strs)}:{"".join(strs)}"
+
+    def decode(self, s: str) -> List[str]:
+        len_strs, message = s.split(":", maxsplit=1) # max_split = 1
+        if not len_strs:
+            return []
+        len_strs = [int(l) for l in len_strs.split(",")]
+
+        decoded = []
+        pointer = 0
+        for len_str in len_strs:
+            decoded.append(message[pointer:pointer+len_str])
+            pointer += len_str
+
+        return decoded
