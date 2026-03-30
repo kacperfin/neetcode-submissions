50ff6a8fd3909506c821b53fffc66d1c42a8aa02
Kacper Aleksander <122747592+kacperfin@users.noreply.github.com>
2026-03-31 01:06:29 +0200

    Add: string-encode-and-decode - submission-11

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-11.py
@@ -0,0 +1,20 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        return "".join((f'{len(string)}:{string}' for string in strs))
+
+    def decode(self, s: str) -> List[str]:
+        decoded = []
+
+        i = 0
+        j = 0
+        while j < len(s):
+            if s[j] == ':':
+                length = int(s[i:j])
+                word = s[j+1:j+1+length]
+                decoded.append(word)
+                i = j + 1 + length
+                j = i
+            else:
+                j += 1
+        return decoded
