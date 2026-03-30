aaa929cab9231bcf246b75d687aebb6deddc654e
Kacper Aleksander <122747592+kacperfin@users.noreply.github.com>
2026-03-30 23:05:23 +0200

    Remove: anagram-groups - submission-1

--- Data Structures & Algorithms/anagram-groups/submission-1.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        d = defaultdict(list)
-        for string in strs:
-            l = [0] * 26  
-            for character in string:
-                l[ord(character) - ord('a')] += 1
-            d[tuple(l)].append(string)
-        return list(d.values())
